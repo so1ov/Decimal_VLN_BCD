01473397064a5e5007481ce96e835bc596425ae8
Artur Soloviev <soloviev.artur@gmail.com>
2019-05-06 15:52:20 +0300

    CMakeLists, VLN class stub and directory hierarchy

---
+++ include/VLN.h
@@ -0,0 +1,38 @@
+//
+// Copyright Artur Soloviev 2019
+// soloviev.artur@gmail.com
+//
+// VLN.h
+//
+// This file is part of VLN Toolkit.
+//
+// VLN Toolkit is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VLN Toolkit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VLN Toolkit.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#ifndef VLN_VLN_H
+#define VLN_VLN_H
+
+namespace sav
+{
+	class VLN
+	{
+		public:
+
+
+		protected:
+
+	};
+}
+
+#endif //VLN_VLN_H
---
+++ src/tests/tests.cpp
@@ -0,0 +1,30 @@
+//
+// Copyright Artur Soloviev 2019
+// soloviev.artur@gmail.com
+//
+// tests.cpp
+//
+// This file is part of VLN Toolkit.
+//
+// VLN Toolkit is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VLN Toolkit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VLN Toolkit.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, world!" << std::endl;
+
+	return 0;
+}
