561d1981865babbda5cfb3655141480688398518
Artur Soloviev <soloviev.artur@gmail.com>
2019-05-19 16:03:15 +0300

    Unified arithmetic fixtures

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -57,126 +57,6 @@ public:
 	}
 };
 
-class DecimalDivisionTests
-	:	public ::testing::Test
-{
-public:
-	void SetUp()
-	{
-		m_decimal1 = DecimalTestWrapper{};
-		m_decimal2 = DecimalTestWrapper{};
-	}
-
-	void TearDown()
-	{
-
-	}
-
-	DecimalTestWrapper m_decimal1;
-	DecimalTestWrapper m_decimal2;
-};
-
-TEST_F(DecimalDivisionTests, DivideByZero)
-{
-	m_decimal1.SetFromString("1");
-	m_decimal2.SetFromString("0");
-
-	auto result = m_decimal1 / m_decimal2;
-	ASSERT_FALSE(result);
-}
-
-TEST_F(DecimalDivisionTests, DivideLess)
-{
-	m_decimal1 = DecimalTestWrapper{"10"};
-	m_decimal2 = DecimalTestWrapper{"15"};
-
-	auto result = m_decimal1 / m_decimal2;
-	ASSERT_TRUE(result.has_value());
-	ASSERT_EQ(result->Quotient, sav::Decimal{});
-	ASSERT_EQ(result->Remainder, m_decimal1);
-}
-
-TEST_F(DecimalDivisionTests, DivideEqual)
-{
-	m_decimal1 = DecimalTestWrapper{"15"};
-	m_decimal2 = DecimalTestWrapper{"15"};
-
-	auto result = m_decimal1 / m_decimal2;
-	ASSERT_TRUE(result.has_value());
-	ASSERT_EQ(result->Quotient, sav::Decimal{"1"});
-	ASSERT_EQ(result->Remainder, sav::Decimal{"0"});
-}
-
-TEST_F(DecimalDivisionTests, DivideGreater)
-{
-	m_decimal1 = DecimalTestWrapper{"16"};
-	m_decimal2 = DecimalTestWrapper{"15"};
-
-	auto result = m_decimal1 / m_decimal2;
-	ASSERT_TRUE(result.has_value());
-	ASSERT_EQ(result->Quotient, sav::Decimal{"1"});
-	ASSERT_EQ(result->Remainder, sav::Decimal{"1"});
-}
-
-TEST_F(DecimalDivisionTests, DivideGreaterByExp1WithoutRemainder)
-{
-	m_decimal1 = DecimalTestWrapper{"1500"};
-	m_decimal2 = DecimalTestWrapper{"15"};
-
-	auto result = m_decimal1 / m_decimal2;
-	ASSERT_TRUE(result.has_value());
-	ASSERT_EQ(result->Quotient, sav::Decimal{"100"});
-	ASSERT_EQ(result->Remainder, sav::Decimal{"0"});
-}
-
-TEST_F(DecimalDivisionTests, DivideGreaterByExp1WithRemainder)
-{
-	m_decimal1 = DecimalTestWrapper{"1501"};
-	m_decimal2 = DecimalTestWrapper{"15"};
-
-	auto result = m_decimal1 / m_decimal2;
-	ASSERT_TRUE(result.has_value());
-	ASSERT_EQ(result->Quotient, sav::Decimal{"100"});
-	ASSERT_EQ(result->Remainder, sav::Decimal{"1"});
-}
-
-TEST_F(DecimalDivisionTests, DivideGreaterBy10thExp2WithoutRemainder)
-{
-	m_decimal1 = DecimalTestWrapper{"15000"};
-	m_decimal2 = DecimalTestWrapper{"15"};
-
-	auto result = m_decimal1 / m_decimal2;
-	ASSERT_TRUE(result.has_value());
-	ASSERT_EQ(result->Quotient, sav::Decimal{"1000"});
-	ASSERT_EQ(result->Remainder, sav::Decimal{"0"});
-}
-
-TEST_F(DecimalDivisionTests, DivideGreaterBy10thExp2WithRemainder)
-{
-	m_decimal1 = DecimalTestWrapper{"15001"};
-	m_decimal2 = DecimalTestWrapper{"15"};
-
-	auto result = m_decimal1 / m_decimal2;
-	ASSERT_TRUE(result.has_value());
-	ASSERT_EQ(result->Quotient, sav::Decimal{"1000"});
-	ASSERT_EQ(result->Remainder, sav::Decimal{"1"});
-}
-
-TEST_F(DecimalDivisionTests, DivideBinaryGreaterByBinaryExp1WithoutRemainder)
-{
-	// Assume this platform has int max value greater than divident
-	int divisor = 15;
-	int divident = divisor * (std::numeric_limits<std::uint8_t>::max() + 1);
-
-	m_decimal1 = DecimalTestWrapper{std::to_string(divident)};
-	m_decimal2 = DecimalTestWrapper{std::to_string(divisor)};
-
-	auto result = m_decimal1 / m_decimal2;
-	ASSERT_TRUE(result.has_value());
-	ASSERT_EQ(result->Quotient, sav::Decimal{std::to_string(divident / divisor)});
-	ASSERT_EQ(result->Remainder, sav::Decimal{std::to_string(divident % divisor)});
-}
-
 class ArithmeticTests
 	:	public ::testing::Test
 {
@@ -239,6 +119,107 @@ TEST_F(ArithmeticTests, Addition_ByZero)
 
 	auto result = m_decimal1 + m_decimal2;
 	ASSERT_EQ(result, sav::Decimal{"10"});
+}
+
+TEST_F(ArithmeticTests, DivideByZero)
+{
+	m_decimal1.SetFromString("1");
+	m_decimal2.SetFromString("0");
+
+	auto result = m_decimal1 / m_decimal2;
+	ASSERT_FALSE(result);
+}
+
+TEST_F(ArithmeticTests, DivideLess)
+{
+	m_decimal1 = DecimalTestWrapper{"10"};
+	m_decimal2 = DecimalTestWrapper{"15"};
+
+	auto result = m_decimal1 / m_decimal2;
+	ASSERT_TRUE(result.has_value());
+	ASSERT_EQ(result->Quotient, sav::Decimal{});
+	ASSERT_EQ(result->Remainder, m_decimal1);
+}
+
+TEST_F(ArithmeticTests, DivideEqual)
+{
+	m_decimal1 = DecimalTestWrapper{"15"};
+	m_decimal2 = DecimalTestWrapper{"15"};
+
+	auto result = m_decimal1 / m_decimal2;
+	ASSERT_TRUE(result.has_value());
+	ASSERT_EQ(result->Quotient, sav::Decimal{"1"});
+	ASSERT_EQ(result->Remainder, sav::Decimal{"0"});
+}
+
+TEST_F(ArithmeticTests, DivideGreater)
+{
+	m_decimal1 = DecimalTestWrapper{"16"};
+	m_decimal2 = DecimalTestWrapper{"15"};
+
+	auto result = m_decimal1 / m_decimal2;
+	ASSERT_TRUE(result.has_value());
+	ASSERT_EQ(result->Quotient, sav::Decimal{"1"});
+	ASSERT_EQ(result->Remainder, sav::Decimal{"1"});
+}
+
+TEST_F(ArithmeticTests, DivideGreaterByExp1WithoutRemainder)
+{
+	m_decimal1 = DecimalTestWrapper{"1500"};
+	m_decimal2 = DecimalTestWrapper{"15"};
+
+	auto result = m_decimal1 / m_decimal2;
+	ASSERT_TRUE(result.has_value());
+	ASSERT_EQ(result->Quotient, sav::Decimal{"100"});
+	ASSERT_EQ(result->Remainder, sav::Decimal{"0"});
+}
+
+TEST_F(ArithmeticTests, DivideGreaterByExp1WithRemainder)
+{
+	m_decimal1 = DecimalTestWrapper{"1501"};
+	m_decimal2 = DecimalTestWrapper{"15"};
+
+	auto result = m_decimal1 / m_decimal2;
+	ASSERT_TRUE(result.has_value());
+	ASSERT_EQ(result->Quotient, sav::Decimal{"100"});
+	ASSERT_EQ(result->Remainder, sav::Decimal{"1"});
+}
+
+TEST_F(ArithmeticTests, DivideGreaterBy10thExp2WithoutRemainder)
+{
+	m_decimal1 = DecimalTestWrapper{"15000"};
+	m_decimal2 = DecimalTestWrapper{"15"};
+
+	auto result = m_decimal1 / m_decimal2;
+	ASSERT_TRUE(result.has_value());
+	ASSERT_EQ(result->Quotient, sav::Decimal{"1000"});
+	ASSERT_EQ(result->Remainder, sav::Decimal{"0"});
+}
+
+TEST_F(ArithmeticTests, DivideGreaterBy10thExp2WithRemainder)
+{
+	m_decimal1 = DecimalTestWrapper{"15001"};
+	m_decimal2 = DecimalTestWrapper{"15"};
+
+	auto result = m_decimal1 / m_decimal2;
+	ASSERT_TRUE(result.has_value());
+	ASSERT_EQ(result->Quotient, sav::Decimal{"1000"});
+	ASSERT_EQ(result->Remainder, sav::Decimal{"1"});
+}
+
+TEST_F(ArithmeticTests, DivideBinaryGreaterByBinaryExp1WithoutRemainder)
+{
+	// Assume this platform has int max value greater than divident
+	int divisor = 15;
+	int divident = divisor * (std::numeric_limits<std::uint8_t>::max() + 1);
+
+	m_decimal1 = DecimalTestWrapper{std::to_string(divident)};
+	m_decimal2 = DecimalTestWrapper{std::to_string(divisor)};
+
+	auto result = m_decimal1 / m_decimal2;
+	ASSERT_TRUE(result.has_value());
+	ASSERT_EQ(result->Quotient, sav::Decimal{std::to_string(divident / divisor)});
+	ASSERT_EQ(result->Remainder, sav::Decimal{std::to_string(divident % divisor)});
 }
 
 int main()
